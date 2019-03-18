03d25fb42f4a7570dacef48d909b4ca044ecd767
Caramelo18 <fabiocaramelo18@gmail.com>
2019-03-18 12:27:51 +0000

    finished merge between LUG11 and SSEC

---
+++ utils.py
@@ -0,0 +1,53 @@
+import pandas
+
+basepath = 'data/'
+
+def merge_ss():
+    filepath = basepath + 'subsections.csv'
+    df = pandas.read_csv(filepath)
+    df['SEC-SS'] = df['SEC11'].map(str) + "-" + df['SS11'].map(str)
+
+    places = {}
+    for _, row in df.iterrows():
+        place_id = row['LUG11']
+        if place_id in places:
+            places[place_id].append(row['SEC-SS'])
+        else:
+            places[place_id] = [row['SEC-SS']]
+
+    filepath = basepath + '2011census-full.csv'
+    df = pandas.read_csv(filepath, encoding="latin1")
+
+    for index, row in df.iterrows():
+        cod = row['GEO_COD']
+        cod = cod[1:]
+        fr = cod[4:6]
+        sec = str(int(cod[6:9]))
+        subsec = str(int(cod[9:11]))
+        df.loc[index, 'GEO_COD'] = cod
+        df.loc[index, 'FR'] = fr
+        df.loc[index, 'SEC-SS'] = sec + "-" + subsec
+    
+    df = df[df['FR'] == '09'] 
+
+    df['LUG11'] = 'def'
+
+    for place in places:
+        subsections = places[place]
+        for subsection in subsections:
+            df.loc[df['SEC-SS'] == subsection, ['LUG11']] = place
+    
+    df = df.drop('ANO', 1)
+    df = df.drop('NIVEL', 1)
+    df = df.groupby(['LUG11']).sum()
+
+    df.to_csv('data/census-lug11.csv')
+    
+    print(df)
+
+
+def main():
+    merge_ss()
+
+if __name__== "__main__":
+    main()
