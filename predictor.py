9f9109372e1334450d7e7d71a689e2f4d5e0b47b
Caramelo18 <fabiocaramelo18@gmail.com>
2019-05-07 19:48:06 +0100

    work on predictor

--- predictor.py
+++ predictor.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import KFold, cross_val_score, cross_val_predict
 from sklearn import linear_model
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import r2_score
 import Parser
 import numpy as np
 
@@ -25,14 +25,26 @@ population_data = np.array(pop_data)
 # print(pop_data)
 
 k_fold = KFold(n_splits=4)
+lasso = linear_model.Lasso(max_iter=20000)
 
-lasso = linear_model.Lasso(max_iter=10000)
-y_pred = cross_val_predict(lasso, X = population_data, y = mortality_data, cv = 7)
-print(mortality_data)
-print(y_pred)
+for train_index, test_index in k_fold.split(population_data):
+#     print("train indices:", train_index)
+#     print("train data:", population_data[train_index])
+#     print("test indices:", test_index)
+#     print("test data:", population_data[test_index])
+    lasso.fit(population_data[train_index], mortality_data[train_index])
 
-accuracy = accuracy_score(y_pred.astype(int), mortality_data.astype(int))
-print(accuracy)
+# pred = cross_val_predict(lasso, population_data, mortality_data, cv=7)
+# accuracy = accuracy_score(pred.astype(int), mortality_data.astype(int))
+# print(accuracy)
 
-# x = lasso.predict(mortality_data[2])
-# print(x)
+predictions = []
+
+for i in range(len(population_data)):
+    data = population_data[i]
+    pred = int(lasso.predict([data]))
+    predictions.append(pred)
+    print(pred, mortality_data[i])
+
+score = r2_score(mortality_data, predictions)
+print(score)
